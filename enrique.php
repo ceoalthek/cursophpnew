7a741e882a6f86d4dd1f98550f228c12de6b68ae
crashkike <enrique.acevedoc@gmail.com>
2018-07-10 12:04:47 -0500

    documentacion de funciones

--- enrique.php
+++ enrique.php
@@ -1,21 +1,25 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
-define('CONSTANT',1);
-define('_CONSTANT',1);
-
-define('EMPTY','');
-
-echo EMPTY;
 /*
-if(!empty(EMPTY)){
-	if(!(boolean) _CONSTANT){
-		print "ONE!";
+Funciones:
+	func_num_args : Numero total de argumentos
+	func_get_arg : devuelve el valor del indice que se le indique
+	func_get_args :  devuelve un array con todos los argumentos
+
+ */
+
+function numero_argumentos(){
+	$numargs  = func_num_args();
+	echo "numero de argumento: $numargs <br>";
+}
+
+function muestra_argumentos(){
+	$numargs  = func_num_args();
+	$arg_list = func_get_args();
+	for($x=0; $x< $numargs; $x++){
+		echo "El argumento $x es : " . $arg_list[$x] . " <br>";
 	}
 }
-else if(constant('CONSTANT') == 1){
-	print "two2";
-}
-*/
+
+numero_argumentos(1,2,4);
+muestra_argumentos(1,23,54);
 ?>
