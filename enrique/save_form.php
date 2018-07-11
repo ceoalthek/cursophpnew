bf6e21189a9fdb726e192d7b0ee9ae3761d7dcbc
Oscar Garcés <oscar@mail-ta.net>
2018-07-11 10:46:36 -0500

    Modificaciones en Ejercicio de Irving

--- enrique/save_form.php
+++ enrique/save_form.php
@@ -11,7 +11,6 @@ function guarda_nombre(){
 	$sql = "INSERT INTO registros (id, nombre, paterno, materno) values (null, '".$_POST['nombre'][0]."', '".$_POST['paterno'][0]."', '".$_POST['materno'][0]."')";
 	mysql_query($sql);
 
-
 	$array = array(
 		'nombre' => $_POST['nombre'][0],
 		'paterno' => $_POST['paterno'][0],
@@ -23,7 +22,7 @@ function guarda_nombre(){
 }
 
 function conecta(){
-	$conexion = mysql_connect("192.168.33.22", "root", "12345678");
+	$conexion = mysql_connect("127.0.0.1", "root", "12345678");
 	$bd = mysql_select_db("test", $conexion);
 
 
--- irving/ejercicio2/login.php
+++ irving/ejercicio2/login.php
@@ -1,7 +1,7 @@
 <?php 
-	$servidor = "localhost";
+	$servidor = "127.0.0.1";
 	$usuario = "root";
-	$contrasena = "";
+	$contrasena = "12345678";
 	$BD = "ejercicio2";
 	
 	$conexion = new mysqli($servidor, $usuario, $contrasena, $BD);
@@ -18,9 +18,9 @@
 				setcookie("user", $res["user"], time() + 3600);
 				setcookie("pass", $res["pass"], time() + 3600);
 				echo $_COOKIE["user"] . " " . $_COOKIE["pass"];
-				header("Location: http://" . $_SERVER['HTTP_HOST'] . "/ejercicio2/form.php");
+				header("Location: http://env.local.com/cursophpnew/irving/ejercicio2/form.php");
 			}else{
-				header("Location: http://" . $_SERVER['HTTP_HOST'] . "/ejercicio2");
+				header("Location: http://env.local.com/cursophpnew/irving/ejercicio2/index.php");
 			}
 		}
 	}
