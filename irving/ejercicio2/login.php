46bb3ab1a0b7208c327a038b2e45b7d2df3670eb
irvingdan <irvingdan@bitbucket.org>
2018-07-11 09:33:51 -0500

    subi ejercicio 2

---
+++ irving/ejercicio2/login.php
@@ -0,0 +1,28 @@
+<?php 
+	$servidor = "localhost";
+	$usuario = "root";
+	$contrasena = "";
+	$BD = "ejercicio2";
+	
+	$conexion = new mysqli($servidor, $usuario, $contrasena, $BD);
+
+	function login($conexion){
+		$res = $conexion->query("CALL login('" . $_POST['usuario'] . "', '" . $_POST['pass'] . "');");
+
+		if( !$res ){
+			echo "Falló la llamada : (" . $conexion->errno . ") " . $conexion->error;
+		}else{
+			//var_dump(mysqli_fetch_assoc($res));
+			$res = mysqli_fetch_assoc($res);
+			if ( $res ){
+				setcookie("user", $res["user"], time() + 3600);
+				setcookie("pass", $res["pass"], time() + 3600);
+				echo $_COOKIE["user"] . " " . $_COOKIE["pass"];
+				header("Location: http://" . $_SERVER['HTTP_HOST'] . "/ejercicio2/form.php");
+			}else{
+				header("Location: http://" . $_SERVER['HTTP_HOST'] . "/ejercicio2");
+			}
+		}
+	}
+	login($conexion);
+?>
---
+++ irving/ejercicio2/sube_archivo.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<?php
+	function sube_archivo(){
+		$dir_subida = 'archivos/';
+		$fichero_subido = $dir_subida . basename($_FILES['archivo']['name']);
+
+		echo '<pre>';
+		if (move_uploaded_file($_FILES['archivo']['tmp_name'], $fichero_subido)) {
+			if( sube_datos($fichero_subido) )
+		    	echo "El fichero es válido y se subió con éxito.\n";
+		    else
+		    	echo "Falló la llamada...";
+		} else {
+		    echo "¡Posible ataque de subida de ficheros!\n";
+		}
+
+		echo 'Más información de depuración:';
+		print_r($_FILES);
+
+		print "</pre>";
+	}
+	sube_archivo();
+
+	function sube_datos($fichero){
+
+		$servidor = "localhost";
+		$usuario = "root";
+		$contrasena = "";
+		$BD = "ejercicio2";
+		
+		$conexion = new mysqli($servidor, $usuario, $contrasena, $BD);
+
+		$res = $conexion->query("CALL insert_captura('" . $_POST['nombre'] . "', '" . $fichero . "');");
+
+		if( !$res ){
+			return false;
+		}else{
+			return true;
+		}
+	}
+?>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="form.php" method="post" accept-charset="utf-8">
+		<div>
+			<button type="submit" href>regresar</button>
+		</div>
+	</form>
+</body>
+</html>
