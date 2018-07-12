2b3bc7f15574fdb7e1267fac38526ba716291121
Oscar Garcés <oscar@mail-ta.net>
2018-07-11 23:50:52 -0500

    Webservices

---
+++ webservices/cliente.php
@@ -0,0 +1,29 @@
+<?php
+    include("nusoap.php");
+
+    $cliente = new nusoap_client("http://www.tecnologiasdealtamira.com/edu/server.php");
+      
+    $error = $cliente->getError();
+    if ($error) {
+        echo "<h2>Constructor error</h2><pre>" . $error . "</pre>";
+    }
+      
+    $result = $cliente->call("getProd", array("categoria" => "libros"));
+      
+    if ($cliente->fault) {
+        echo "<h2>Fault</h2><pre>";
+        print_r($result);
+        echo "</pre>";
+    }
+    else {
+        $error = $cliente->getError();
+        if ($error) {
+            echo "<h2>Error</h2><pre>" . $error . "</pre>";
+        }
+        else {
+            echo "<h2>Libros</h2><pre>";
+            echo $result;
+            echo "</pre>";
+        }
+    }
+?>
