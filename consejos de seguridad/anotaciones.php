00425ff780f968e7a91cde2cc2b5632ec65abc3c
Oscar Garcés <oscar@mail-ta.net>
2018-07-12 13:11:46 -0500

    Consejos de Seguridad

--- consejos de seguridad/anotaciones.php
+++ consejos de seguridad/anotaciones.php
@@ -11,20 +11,39 @@ foreach($_POST as $key=>value)
 	$_POST[$key] = str_replace('"','',$value)
 }
 
-===============================================
-2. Guardar contraseñas e emails en HASH md5 o SHA1
+Certificados SSL
+- DV Domain Validation
+- OV Operation Validation
+- EV Extended Validation
 
 ===============================================
-3. Uso de Variables de Sesion Encriptadas y no de Cookies
+2. Guardar contraseñas e emails, usernames en HASH md5 (32 caracteres) o SHA1 (40 caracteres)
+
+===============================================
+3. Uso de Variables de Sesion Encriptadas y no de Cookies (cuando es necesario que utilicemos sesiones)
+
 
 ===============================================
 4. Proteger suplantación de Sesiones
 
 session_start();
-$_SESSION['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'];
-$_SESSION['HTTP_USER_AGENT'] = $_SERVER['HTTP_USER_AGENT'];
+$_SESSION['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR']; //IP del visitante
+$_SESSION['HTTP_USER_AGENT'] = $_SERVER['HTTP_USER_AGENT']; //Explorador de Internet
+
 
 ===============================================
 5. Parámetros GET y POST explotables
 
-http://www.e-commerce.com/index.ptp?idproducto=15
+http://www.e-commerce.com/index.ptp?idproducto=15 Dirija a un 404 si yo edito la URL
+
+===============================================
+EJERCICIO
+- Formulario de Login (username, password)
+- Hash (md5 o sha1) de ambos fields
+- Ingrese a la DB la IP del visitante y el Explorador de Internet
+- Elimine apóstrofes o dobles comillas de los inputs
+- Puede utilizar PDO o Consultas MySQL Estándar
+
+
+
+
