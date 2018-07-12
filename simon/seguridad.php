50d3708aa596052b97028d8f3b1a67f1d69fc444
simonhg <simonhg21@gmail.com>
2018-07-12 13:43:15 -0500

    ejercicio seguridad

---
+++ simon/seguridad.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Ejercicio de seguridad
+	</title>
+</head>
+<body>
+		
+	<form name = "seguridad" action="save_seguridad.php" method="post" role="form" enctype ="multipart/form-data">
+		<input type="text" name="usuario">
+		<input type="password" name="pass">
+		<button type ="submit">Guardar</button>
+	</form>
+</body>
+</html>
